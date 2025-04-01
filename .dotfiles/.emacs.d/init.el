a9c1c33b5c5f21ae3d31b4698f4352b9f1b17ea0
Steven Schaefer <stschaef@umich.edu>
2025-04-01 12:04:49 -0400

    nix agda sucks

--- .dotfiles/.emacs.d/init.el
+++ .dotfiles/.emacs.d/init.el
@@ -31,6 +31,14 @@
 
 (use-package org-agenda
  :ensure org)
+
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode)
+)
+(setq evil-undo-system 'undo-tree)
+
 (use-package evil
  :ensure t
  :init
@@ -43,7 +51,7 @@
    (evil-mode 1)
 )
 (use-package evil-surround
-  :ensure t
+  :ensure
   :config
   (global-evil-surround-mode 1))
 (use-package vimish-fold
@@ -61,6 +69,10 @@
 
 (use-package flycheck)
 
+(use-package consult :ensure t)
+
+(setq mac-command-modifier 'meta)
+
 (use-package magit
   :ensure t
   :config
@@ -74,6 +86,10 @@
   :config
  (evil-collection-init 'magit)) ; This initializes the evil bindings for magit
 
+(use-package evil-commentary
+  :ensure t
+  :init (evil-commentary-mode 1)
+ )
 
 (use-package ivy
   :ensure t
@@ -81,17 +97,31 @@
   (ivy-mode 1)
 )
 
+
+(setq ivy-extra-directories ())
+
 (use-package counsel
   :ensure t
   :after ivy
+  :bind (("M-x" . counsel-M-x)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
   :config
   (counsel-mode 1))
 
+(use-package ivy-rich
+  :ensure t
+  :init (ivy-rich-mode 1))
+
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
 ;; Configure the content of the dashboard
-(setq dashboard-items '((recents . 5)
-                        (bookmarks . 5)
-                        (projects . 5)
-                        (agenda . 5)
+(setq dashboard-items '((recents . 10)
+                        (projects . 10)
       ))
 
 (use-package projectile
@@ -106,33 +136,247 @@
   :config
   (setq which-key-idle-delay 0.3))
 
-(use-package company)
-
 (use-package doom-themes
   :ensure t
   :config
   (load-theme 'doom-challenger-deep t))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
 
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(set-frame-font "JetBrains Mono-16:weight=medium" nil t)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+(global-auto-revert-mode 1)
 (line-number-mode 1)
 (column-number-mode 1)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(use-package general)
+(set-frame-font "JetBrains Mono-14:weight=medium" nil t)
 
-(require 'general)
-(general-define-key
- :states '(normal visual)
- :keymaps 'override 
- :prefix "SPC"
-  "SPC"   '(counsel-M-x :which-key "M-x")
-  "."     '(counsel-find-file :which-key "Find file")
-  ","     '(ivy-switch-buffer :which-key "Switch Buffer")
-  "f r"   '(counsel-recentf :which-key "Recent files")
-  "w" evil-window-map
-  "s p" `my/projectile-ripgrep
-  "p" projectile-mode-map
-  "g" magit-mode-map
-  "t t"   '(toggle-truncate-lines :which-key "Toggle truncate lines"))
+
+(use-package company
+  :ensure t
+  :config (global-company-mode 1))
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package hl-todo :ensure t :init (hl-todo-mode 1))
+(use-package unicode-fonts :ensure t)
+
+(defun kill-other-buffers ()
+     "Kill all other buffers."
+     (interactive)
+     (mapc #'kill-buffer (delq (current-buffer) (buffer-list))))
+
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
+(defun move-this-file (new-path)
+  "Move current buffer's file to NEW-PATH."
+  (interactive
+   (list (read-file-name "Move file to: ")))
+  (unless (and buffer-file-name (file-exists-p buffer-file-name))
+    (user-error "Buffer is not visiting any file"))
+  (let ((old-path (buffer-file-name))
+        (new-path (expand-file-name new-path)))
+    (when (directory-name-p new-path)
+      (setq new-path (concat new-path (file-name-nondirectory old-path))))
+    (make-directory (file-name-directory new-path) 't)
+    (rename-file old-path new-path 1)
+    (set-visited-file-name new-path t t)
+    (message "File moved to %S" (abbreviate-file-name new-path))))
+
+(defun copy-this-file (new-path)
+  "Copy current buffer's file to NEW-PATH then open NEW-PATH."
+  (interactive
+   (list (read-file-name "Copy file to: ")))
+  (unless (and buffer-file-name (file-exists-p buffer-file-name))
+    (user-error "Buffer is not visiting any file"))
+  (let ((old-path (buffer-file-name))
+        (new-path (expand-file-name new-path)))
+    (make-directory (file-name-directory new-path) 't)
+    (copy-file old-path new-path 1)
+    (find-file new-path)
+    (message "File copied to %S" (abbreviate-file-name new-path))))
+
+(defun delete-this-file (&optional path)
+  "Delete PATH, kill its buffers.
+If PATH is not specified, default to the current buffer's file."
+  (interactive
+   (list (buffer-file-name)))
+  (let* ((path (or path (buffer-file-name)))
+         (short-path (and path (abbreviate-file-name path))))
+    (unless path
+      (user-error "Buffer is not visiting any file"))
+    (unless (file-exists-p path)
+      (error "File doesn't exist: %s" path))
+    (unless (y-or-n-p (format "Really delete %S?" short-path))
+      (user-error "Aborted"))
+    (let ((buf (current-buffer)))
+      (unwind-protect
+          (progn (delete-file path t) t)
+        (if (file-exists-p path)
+            (error "Failed to delete %S" short-path)
+          ;; Kill all buffers visiting this file
+          (dolist (buffer (buffer-list))
+            (when (string= (buffer-file-name buffer) path)
+              (kill-buffer buffer)))
+          (message "Deleted %S" short-path))))))
+
+(use-package general
+  :config
+  (general-evil-setup t)
+  (defconst my-leader "SPC")
+  (general-create-definer my-leader-def
+    :prefix my-leader)
+  (general-override-mode) ;; https://github.com/noctuid/general.el/issues/99#issuecomment-360914335
+  ;; doomesque hotkeys using spacebar as prefix
+  (my-leader-def
+    :states '(motion normal visual)
+    :keymaps 'override ;; https://github.com/noctuid/general.el/issues/99#issuecomment-360914335
+
+    ;; map universal argument to SPC-u
+    "u" '(universal-argument :which-key "Universal argument")
+    ";" '(eval-region :which-key "eval-region")
+    "SPC" '(projectile-find-file :which-key "Projectile find file")
+    "C-SPC" '(projectile-find-file-other-frame :which-key "Projectile find file (new frame)")
+    "S-SPC" '(projectile-find-file-other-frame :which-key "Projectile find file (new frame)")
+    "." '(find-file :which-key "Find file")
+    ">" '(find-file-other-frame :which-key "Find file (new frame)")
+    "," '(consult-buffer :which-key "consult-buffer")
+    ":" '(execute-extended-command :which-key "M-x")
+    "x" '(open-scratch-buffer :which-key "Open scratch buffer")
+    "d" '(dired-jump :which-key "dired-jump")
+    "/" '(+consult/ripgrep :which-key "+consult/ripgrep")
+    "?" '(consult-ripgrep :which-key "consult-ripgrep")
+    "v" '(vterm-toggle :which-key "vterm-toggle")
+
+    ;; editor
+    "e" '(:ignore t :which-key "Editor")
+    "eu" '(vundo :which-key "vundo")
+    "ev" '(vundo :which-key "vundo")
+    "er" '(query-replace :which-key "query-replace")
+    ;"ec" '(consult-theme :which-key "consult-theme")
+    "ep" '(point-to-register :which-key "point-to-register")
+    "es" '(consult-register-store :which-key "consult-register-store")
+    "ej" '(jump-to-register :which-key "jump-to-register")
+    "ef" '(:ignore t :which-key "Fold")
+    "efh" '(hs-hide-block :which-key "hs-hide-block")
+    "efs" '(hs-show-block :which-key "hs-show-block")
+    "efa" '(hs-show-all :which-key "hs-show-all")
+
+    ;; consult
+    "c" '(:ignore t :which-key "consult")
+    "cf" '(consult-flymake :which-key "consult-flymake")
+    "ct" '(consult-theme :which-key "consult-theme")
+
+
+    "w" evil-window-map
+    "fr"   '(counsel-recentf :which-key "Recent files")
+    "fR"   '(move-this-file :which-key "Move current file")
+    "fD"   '(delete-this-file :which-key "Delete current file")
+    "fC"   '(copy-this-file :which-key "Copy current file")
+
+    ;; buffer
+    "b" '(:ignore t :which-key "Buffer")
+    "bb" '(consult-buffer :which-key "consult-buffer")
+    "b[" '(previous-buffer :which-key "Previous buffer")
+    "b]" '(next-buffer :which-key "Next buffer")
+    "bd" '(kill-current-buffer :which-key "Kill buffer")
+    "bk" '(kill-current-buffer :which-key "Kill buffer")
+    "bl" '(evil-switch-to-windows-last-buffer :which-key "Switch to last buffer")
+    "br" '(revert-buffer-no-confirm :which-key "Revert buffer")
+    "bK" '(kill-other-buffers :which-key "Kill other buffers")
+
+    ;; open
+    "o" '(:ignore t :which-key "Open")
+    "oc" '(open-init-file :which-key "Open init.el")
+
+    ;; project
+    "p" '(:ignore t :which-key "Project")
+    "pp" '(projectile-switch-project :which-key "Switch Project")
+    "po" '(projectile-find-other-file :which-key "projectile-find-other-file")
+
+    ;; help
+    "h" '(:ignore t :which-key "Help")
+    "hf" '(helpful-callable :which-key "describe-function")
+    "hk" '(helpful-key :which-key "describe-key")
+    "hv" '(helpful-variable :which-key "describe-variable")
+    "ho" '(helpful-symbol :which-key "describe-symbol")
+    "hm" '(describe-mode :which-key "describe-mode")
+    "hF" '(describe-face :which-key "describe-face")
+    "hw" '(where-is :which-key "where-is")
+    "h." '(display-local-help :which-key "display-local-help")
+
+    ;; toggles
+    "t" '(:ignore t :which-key "Toggles")
+    "ts" '(flyspell-mode :which-key "flyspell-mode")
+    "tf" '(flyspell-mode :which-key "flyspell-mode")
+    "tc" '(flymake-mode :which-key "flymake-mode")
+    "tg" '(evil-goggles-mode :which-key "evil-goggles")
+    "tI" '(toggle-indent-style :which-key "Indent style")
+    "tv" '(visual-line-mode :which-key "visual-line-mode")
+
+    ;; tabs
+    "TAB" '(:ignore t :which-key "Tabs")
+    "TAB TAB" '(tab-bar-switch-to-tab :which-key "tab-bar-switch-to-tab")
+    "TAB [" '(+tab-bar/switch-to-prev-tab :which-key "+tab-bar/switch-to-prev-tab")
+    "TAB ]" '(t+ab-bar/switch-to-next-tab :which-key "+tab-bar/switch-to-next-tab")
+    "TAB n" '(+tab-bar/add-new :which-key "+tab-bar/add-new")
+    "TAB k" '(+tab-bar/close-tab :which-key "+tab-bar/close-tab")
+    "TAB d" '(+tab-bar/close-tab :which-key "+tab-bar/close-tab")
+    "TAB K" '(+tab-bar/close-all-tabs-except-current :which-key "+tab-bar/close-all-tabs-except-current")
+    "TAB r" '(tab-rename :which-key "tab-rename")
+
+    ;; quick tab switching
+    "1" '((lambda () (interactive) (+tab-bar/switch-by-index 0)) :which-key nil)
+    "2" '((lambda () (interactive) (+tab-bar/switch-by-index 1)) :which-key nil)
+    "3" '((lambda () (interactive) (+tab-bar/switch-by-index 2)) :which-key nil)
+    "4" '((lambda () (interactive) (+tab-bar/switch-by-index 3)) :which-key nil)
+    "5" '((lambda () (interactive) (+tab-bar/switch-by-index 4)) :which-key nil)
+    "6" '((lambda () (interactive) (+tab-bar/switch-by-index 5)) :which-key nil)
+    "7" '((lambda () (interactive) (+tab-bar/switch-by-index 6)) :which-key nil)
+    "8" '((lambda () (interactive) (+tab-bar/switch-by-index 7)) :which-key nil)
+    "9" '((lambda () (interactive) (+tab-bar/switch-by-index 8)) :which-key nil)
+
+    ;; git
+    "g" '(:ignore t :which-key "Git") ; prefix
+    "gg" '(magit-status :which-key "Git status")
+
+    "qq" '(save-buffers-kill-terminal :which-key "Quit Emacs")
+    "qr" '(restart-emacs :which-key "Restart Emacs")
+    )
+
+  ;; magit
+  (general-define-key
+    ;; https://github.com/emacs-evil/evil-magit/issues/14#issuecomment-626583736
+    :keymaps 'transient-base-map
+    "<escape>" 'transient-quit-one)
+
+  ;; magit keybindings
+  ;; TODO refactor within use-package
+  (general-define-key
+    :states '(normal visual)
+    :keymaps 'magit-mode-map
+    ;; rebind "q" in magit-status to kill the magit buffers instead of burying them
+    "q" '+magit/quit
+
+    ;; tab switching within magit
+    "M-1" (lambda () (interactive) (+tab-bar/switch-by-index 0))
+    "M-2" (lambda () (interactive) (+tab-bar/switch-by-index 1))
+    "M-3" (lambda () (interactive) (+tab-bar/switch-by-index 2))
+    "M-4" (lambda () (interactive) (+tab-bar/switch-by-index 3))
+    "M-5" (lambda () (interactive) (+tab-bar/switch-by-index 4))
+    "M-6" (lambda () (interactive) (+tab-bar/switch-by-index 5))
+    "M-7" (lambda () (interactive) (+tab-bar/switch-by-index 6))
+    "M-8" (lambda () (interactive) (+tab-bar/switch-by-index 7))
+    "M-9" (lambda () (interactive) (+tab-bar/switch-by-index 8))))
+
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
--- hosts/schmapple/home.nix
+++ hosts/schmapple/home.nix
@@ -29,13 +29,48 @@
     packages = with pkgs; [
       raycast
       aerospace
+      (agda.withPackages (ps: [
+        ps.standard-library
+        # (ps.mkDerivation {
+        #   pname = "cubical";
+        #   version = "0.7";
+        #   src = "/Users/stevenschaefer/cubical/";
+        #   libraryFile = "cubical.agda-lib";
+        #   libraryName = "cubical";
+        #   buildPhase = '''';
+        #   dontUnpack = true;
+        #   meta = {
+        #     description = "Cubical library";
+        #     license = lib.licenses.mit;  # Adjust according to the actual license
+        #   };
+        # })
+        # (ps.mkDerivation {
+        #   pname = "cubical-categorical-logic";
+        #   version = "1.0.0";
+        #   src = "/Users/stevenschaefer/cubical-categorical-logic/";
+        #   libraryFile = "cubical-categorical-logic.agda-lib";
+        #   libraryName = "cubical-categorical-logic";
+        #   buildPhase = '''';
+        #   dontUnpack = true;
+        #   meta = {
+        #     description = "Cubical categorical logic library";
+        #     license = lib.licenses.mit;  # Adjust according to the actual license
+        #   };
+        # })
+      ]))
     ];
     file = {
     ".config/aerospace" = {
       source = ./aerospace;
       recursive = true;
     };
+    ".agda/libraries".text = ''
+      /Users/stevenschaefer/cubical/cubical.agda-lib
+      /Users/stevenschaefer/agda-stdlib/standard-library.agda-lib
+      /Users/stevenschaefer/cubical-categorical-logic/cubical-categorical-logic.agda-lib
+    '';
     };
+
     sessionVariables = {
       EDITOR = "emacs";
     };
@@ -56,7 +91,7 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # imports = [ 
+  # imports = [
   #   (import ../../emacs.nix {
   #     inherit config pkgs lib;
   #     emacsPackage = homebrewEmacs;
