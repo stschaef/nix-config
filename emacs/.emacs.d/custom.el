4a985e2580462ba443c0ef585158fe18d4535836
Steven Schaefer <stschaef@umich.edu>
2026-01-10 20:40:27 -0500

    custom

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,28 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(window-divider-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(agda2-highlight-bound-variable-face ((t (:foreground "#f4b8e4"))))
+ '(agda2-highlight-datatype-face ((t (:foreground "#85c1dc"))))
+ '(agda2-highlight-error-face ((t (:foreground "#a6d189"))))
+ '(agda2-highlight-function-face ((t (:foreground "#f2d5cf"))))
+ '(agda2-highlight-generalizable-variable-face ((t (:foreground "#ea999c"))))
+ '(agda2-highlight-inductive-constructor-face ((t (:foreground "#85c1dc"))))
+ '(agda2-highlight-keyword-face ((t (:foreground "#e78284"))))
+ '(agda2-highlight-module-face ((t (:foreground "#8caaee"))))
+ '(agda2-highlight-number-face ((t (:foreground "#ef9f76"))))
+ '(agda2-highlight-operator-face ((t (:foreground "#81c8be"))))
+ '(agda2-highlight-postulate-face ((t (:foreground "#8caaee"))))
+ '(agda2-highlight-primitive-face ((t (:foreground "#ef9f76"))))
+ '(agda2-highlight-string-face ((t (:foreground "#a6d189"))))
+ '(agda2-highlight-symbol-face ((t (:foreground "#babbf1"))))
+ '(agda2-highlight-typechecks-face ((t (:foreground "#a6d189"))))
+ '(window-divider-first-pixel ((t (:foreground "#292c3c"))))
+ '(window-divider-last-pixel ((t (:foreground "#292c3c")))))
